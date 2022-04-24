4a71b0bfb14e46493c9509344072682ab7da7ecb
Richard Ng <richardcrng@gmail.com>
2022-04-24 19:48:38 +0100

    Format with Prettier

--- server/src/express.ts
+++ server/src/express.ts
@@ -5,7 +5,9 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is the backend server for Cockroach Poker - you might want to try https://cockroach-poker.rcr.dev for something more interesting");
+  res.send(
+    "This is the backend server for Cockroach Poker - you might want to try https://cockroach-poker.rcr.dev for something more interesting"
+  );
 });
 
 app.get("/ping", (req, res) => {
