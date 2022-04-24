f3330af1f8f890339bb89bf478cd32b0f3ec43b6
Richard Ng <richardcrng@gmail.com>
2022-04-24 18:32:18 +0100

    Set up heroku configuration

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,7 +4,7 @@ import { ClientSocket } from "./types/event.types";
 
 export const socketUrl =
   process.env.NODE_ENV === "production"
-    ? "https://two-rooms-and-a-boom-server.herokuapp.com"
+    ? "https://cockroach-poker.herokuapp.com"
     : "http://localhost:4000";
 
 export const socket: ClientSocket = io(socketUrl);
